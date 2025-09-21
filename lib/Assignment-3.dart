1ed0ecf39d58d25562356ce003289397674e3fe9
MD. Alif Bin Saudh <alifhamim50@gmail.com>
2025-09-22 00:02:37 +0600

    Update Assignment-3.dart

--- lib/Assignment-3.dart
+++ lib/Assignment-3.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Assignment 13',
+      title: 'Assignment-3',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -200,3 +200,4 @@ class HomePageCard extends StatelessWidget {
     );
   }
 }
+
